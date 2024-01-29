3981b26de001f60f7d92414c0f73de2b8c039d64
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2024-01-29 14:36:20 +0600

    design done

---
+++ src/react/app.js
@@ -0,0 +1,7 @@
+function App() {
+    return <h1>Hello World!</h1>;
+  }
+  
+  const container = document.getElementById('root');
+  const root = ReactDOM.createRoot(container);
+  root.render(<App />)
